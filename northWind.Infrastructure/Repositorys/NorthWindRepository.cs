52feadb4c62d4f1c0f3e9d4719546f6495e3538d
João Neto <joao.neto96@souunit.com.br>
2023-04-26 21:16:49 -0300

    Refatoração NortWindRepository; Criação de classe de scripts

--- northWind.Infrastructure/Repositorys/NorthWindRepository.cs
+++ northWind.Infrastructure/Repositorys/NorthWindRepository.cs
@@ -2,14 +2,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using northWind.Domain.Entites;
 using northWind.Domain.Repositorys;
-using System;
-using System.Collections.Generic;
+using northWind.Infrastructure.Scripts;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace northWind.Infrastructure.Repositorys
 {
@@ -18,142 +13,56 @@ namespace northWind.Infrastructure.Repositorys
         private readonly IServiceProvider _serviceProvider;
         public NorthWindRepository(IServiceProvider serviceProvider)
         {
-            _serviceProvider= serviceProvider;
+            _serviceProvider = serviceProvider;
         }
 
         public async Task<List<Order>> ObterOrderPorAno(string year)
         {
-            const string sql = "SELECT " +
-                   "YEAR(ShippedDate) AS Year, " +
-                   "O.OrderID AS OrderId, " +
-                   "SUM(UnitPrice * Quantity) AS Subtotal, " +
-                   "ShippedDate " +
-                   "FROM dbo.Orders O INNER JOIN dbo.[Order Details] D ON O.OrderID = D.OrderID " +
-                   "WHERE YEAR(@year) = YEAR(ShippedDate) " +
-                   "AND YEAR(@year) BETWEEN YEAR('1996-12-24') AND YEAR('1997-09-30') " +
-                   "GROUP BY " +
-                   "YEAR(ShippedDate), " +
-                   "O.OrderID, " +
-                   "ShippedDate ";
-
             var parametros = new DynamicParameters();
             parametros.Add("@year", year, DbType.String);
 
-            try
-            {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Order>(sql,parametros)).ToList();
-                return lista;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR" + ex.Message);
-                throw;
-            }
+            List<Order> lista = await Query<Order>(NorthWindScripts.SQL_OBTER_PRODUTO_POR_ANO, parametros);
+            return lista;
         }
 
         public async Task<Order[]> ObterOrders()
         {
-            const string sql = "SELECT TOP 500 " +
-                "OrderID as OrderID, " +
-                "CustomerID as CustomerID, " +
-                "OrderDate as OrderDate, " +
-                "ShippedDate as ShippedDate, " +
-                "ShipName as ShipName" +
-                " FROM dbo.Orders";
-
-            try
-            {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Order>(sql)).ToArray();
-                return lista;
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("ERROR" + ex.Message);
-                throw;
-            }
-
+            var lista = await Query<Order>(NorthWindScripts.SQL_OBTER_ORDERS);
+            return lista.ToArray();
         }
 
         public async Task<List<Product>> ObterProducts(decimal price)
         {
-            const string sql = "SELECT TOP 500 " +
-                               "ProductID as ProductID, " +
-                               "ProductName as ProductName, " +
-                               "UnitPrice as UnitPrice " +
-                               " FROM dbo.Products " +
-                               "WHERE UnitPrice <  @price";
             var parametros = new DynamicParameters();
-                parametros.Add("@price", price, DbType.Decimal);
+            parametros.Add("@price", price, DbType.Decimal);
 
-            try
-            {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Product>(sql , parametros)).ToList();
-                return lista;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR" + ex.Message);
-                throw;
-            }
-
+            var lista = await Query<Product>(NorthWindScripts.SQL_OBTER_PRODUTOS, parametros);
+            return lista;
         }
 
         public async Task<List<Product>> ObterProdutosAcimaDaMediaPreco()
         {
-            const string sql = "SELECT TOP 500 " +
-                               "ProductID as ProductID, " +
-                               "ProductName as ProductName, " +
-                               "UnitPrice as UnitPrice " +
-                               " FROM dbo.Products " +
-                               "WHERE UnitPrice > (SELECT AVG(UnitPrice) From dbo.Products) " +
-                               "ORDER BY UnitPrice ASC";
-            try
-            {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Product>(sql)).ToList();
-                return lista;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR" + ex.Message);
-                throw;
-            }
+            var lista = await Query<Product>(NorthWindScripts.SQL_OBTER_PRODUTO_ACIMA_DA_MEDIA_PRECO);
+            return lista;
         }
 
         public async Task<List<Order>> ObterValorTotalPedido()
         {
-            const string sql = "SELECT OrderID as OrderID, " +
-                               "SUM(UnitPrice * Quantity) as Subtotal " +
-                               "FROM dbo.[Order Details] " +
-                               "GROUP BY OrderID";
-
-            try
-            {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Order>(sql)).ToList();
-                return lista;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR" + ex.Message);
-                throw;
-            }
+            var lista = await Query<Order>(NorthWindScripts.SQL_OBTER_VALOR_TOTAL_PEDIDO);
+            return lista;
         }
 
         public async Task<List<Employees>> ObterVendaPorPais()
         {
-            const string sql = "SELECT E.Country,E.LastName,E.FirstName, " +
-                               "O.ShippedDate,O.OrderID, " +
-                               "ROUND(SUM(D.Quantity*D.UnitPrice),2) AS SaleAmount " +
-                               "FROM dbo.Employees E " +
-                               "INNER JOIN dbo.Orders O " +
-                               "ON E.EmployeeID = O.EmployeeID " +
-                               "INNER JOIN dbo.[Order Details] D " +
-                               "ON O.OrderID = D.OrderID " +
-                               "GROUP BY E.Country,E.LastName,E.FirstName,O.OrderID,O.ShippedDate ";
+            var lista = await Query<Employees>(NorthWindScripts.SQL_OBTER_VENDA_POR_PAIS);
+            return lista;
+        }
 
+        private async Task<List<T>> Query<T>(string sql, DynamicParameters? parametros = null)
+        {
             try
             {
-                var lista = (await _serviceProvider.GetService<SqlConnection>().QueryAsync<Employees>(sql)).ToList();
-                return lista;
+                return (await _serviceProvider.GetService<SqlConnection>().QueryAsync<T>(sql, parametros)).ToList();
             }
             catch (Exception ex)
             {
---
+++ northWind.Infrastructure/Scripts/NorthWindScripts.cs
@@ -0,0 +1,82 @@
+﻿namespace northWind.Infrastructure.Scripts
+{
+    public static class NorthWindScripts
+    {
+        public const string SQL_OBTER_PRODUTO_POR_ANO = @"
+            SELECT
+                YEAR(ShippedDate) AS Year,
+                O.OrderID AS OrderId,
+                SUM(UnitPrice * Quantity) AS Subtotal,
+                ShippedDate
+            FROM 
+                dbo.Orders O 
+            INNER JOIN 
+                dbo.[Order Details] D ON O.OrderID = D.OrderID
+            WHERE 
+                YEAR(@year) = YEAR(ShippedDate)
+                AND YEAR(@year) BETWEEN YEAR('1996-12-24') AND YEAR('1997-09-30')
+            GROUP BY
+                YEAR(ShippedDate),
+                O.OrderID,
+                ShippedDate";
+
+        public const string SQL_OBTER_ORDERS = @"
+            SELECT TOP 500
+                OrderID as OrderID,
+                CustomerID as CustomerID,
+                OrderDate as OrderDate
+                ShippedDate as ShippedDate
+                ShipName as ShipName
+            FROM 
+                dbo.Orders
+        ";
+
+        public const string SQL_OBTER_PRODUTOS = @"
+            SELECT TOP 500
+                ProductID as ProductID,
+                ProductName as ProductName,
+                UnitPrice as UnitPrice
+            FROM 
+                dbo.Products
+            WHERE 
+                UnitPrice <  @price"  
+        ;
+
+        public const string SQL_OBTER_PRODUTO_ACIMA_DA_MEDIA_PRECO = @"
+            SELECT TOP 500
+                ProductID as ProductID,
+                ProductName as ProductName,
+                UnitPrice as UnitPrice
+            FROM 
+                dbo.Products
+            WHERE
+                UnitPrice > (SELECT AVG(UnitPrice) From dbo.Products)
+            ORDER BY 
+                UnitPrice ASC
+        ";
+
+        public const string SQL_OBTER_VALOR_TOTAL_PEDIDO = @"
+            SELECT 
+                OrderID as OrderID,
+                SUM(UnitPrice * Quantity) as Subtotal
+            FROM 
+                dbo.[Order Details]
+            GROUP BY 
+                OrderID
+        ";
+
+        public const string SQL_OBTER_VENDA_POR_PAIS = @"
+            SELECT 
+                E.Country, E.LastName, E.FirstName, O.ShippedDate, O.OrderID,
+                ROUND(SUM(D.Quantity*D.UnitPrice),2) AS SaleAmount
+            FROM 
+                dbo.Employees E
+            INNER JOIN 
+                dbo.Orders O ON E.EmployeeID = O.EmployeeID
+            INNER JOIN 
+                dbo.[Order Details] D ON O.OrderID = D.OrderID
+            GROUP BY 
+                E.Country, E.LastName, E.FirstName, O.OrderID, O.ShippedDate
+        ";
+    }
+}
